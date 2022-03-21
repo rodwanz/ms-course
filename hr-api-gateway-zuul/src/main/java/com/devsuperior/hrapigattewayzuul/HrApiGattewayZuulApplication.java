9bc81a186f7e6fd49614d9c25d3db4f85c8135be
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-03-21 20:41:53 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigattewayzuul/HrApiGattewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrapigattewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGattewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGattewayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigattewayzuul/HrApiGattewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrapigattewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGattewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
