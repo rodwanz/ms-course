0e440cafb155f309755dc39e6e6093ad43fe568f
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-04-18 20:16:46 -0300

    db config server

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResources.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +23,7 @@ public class WorkerResources {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
+
 	@Autowired
 	private Environment env;
 	
@@ -35,7 +32,7 @@ public class WorkerResources {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 		
 	}
@@ -50,12 +47,14 @@ public class WorkerResources {
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
 
+		/*
 		try {
 			Thread.sleep(3000l);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		*/
 
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
